f7a41a09d8a067d6f9debc0ccd730d7ead0da4cb
g4l3win <144231516+g4l3win@users.noreply.github.com>
2025-02-07 05:39:42 +0700

    Update routes.dart

--- lib/navigation/routes.dart
+++ lib/navigation/routes.dart
@@ -1,13 +1,15 @@
  abstract class Routes {
-  static const home =_Paths.home;
+  static const home = _Paths.home;
   static const account = _Paths.account;
   static const browse = _Paths.browse;
   static const settings = _Paths.settings;
- }
+  static const FAQ = _Paths.FAQ;
+}
 
- abstract class _Paths{
+abstract class _Paths {
   static const home = '/home';
   static const account = '/account';
   static const browse = '/browse';
   static const settings = '/settings';
- }
+  static const FAQ = '/FAQ';
+}
