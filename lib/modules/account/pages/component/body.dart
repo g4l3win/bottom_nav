88a8633a0ab5ec8633e079c1193f6e546b4ffd19
g4l3win <144231516+g4l3win@users.noreply.github.com>
2025-02-09 15:30:12 +0700

    Update body.dart

--- lib/modules/account/pages/component/body.dart
+++ lib/modules/account/pages/component/body.dart
@@ -25,41 +25,56 @@ class Body extends GetView<AccountController> {
         //ini textboxnya
         Textbox(
           label: "Nama Pengguna",
+          tekscontroller: controller.nameController,
         ),
         Textbox(
           label: "Email",
+          tekscontroller: controller.emailController,
         ),
         Textbox(
           label: "Tanggal lahir",
+          tekscontroller: controller.birthdateController,
         ),
         Textbox(
           label: "Handphone",
+          tekscontroller: controller.phoneController,
         ),
         Textbox(
           label: "Tinggi",
+          tekscontroller: controller.heightController,
         ),
         Textbox(
           label: "Berat",
+          tekscontroller: controller.weightController,
         ),
         //Row untuk radio button
         Row(
           children: [
-            Text("Jenis Kelamin:", style: TextStyle(fontSize: 15),),
-            Expanded(
-              child: Obx(() => RadioListTile<Gender>(
-                title: Text("L", style: TextStyle(fontSize: 12),),
-                value: Gender.lakiLaki,
-                groupValue: controller.selectedGender.value,
-                onChanged: (value) => controller.setSelected(value!),
-              )),
+            Text(
+              "Jenis Kelamin:",
+              style: TextStyle(fontSize: 15),
             ),
             Expanded(
               child: Obx(() => RadioListTile<Gender>(
-                title: Text("P", style: TextStyle(fontSize: 12),),
-                value: Gender.perempuan,
-                groupValue: controller.selectedGender.value,
-                onChanged: (value) => controller.setSelected(value!),
-              )),
+                    title: Text(
+                      "L",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                    value: Gender.lakiLaki,
+                    groupValue: controller.selectedGender.value,
+                    onChanged: (value) => controller.setSelected(value!),
+                  )),
+            ),
+            Expanded(
+              child: Obx(() => RadioListTile<Gender>(
+                    title: Text(
+                      "P",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                    value: Gender.perempuan,
+                    groupValue: controller.selectedGender.value,
+                    onChanged: (value) => controller.setSelected(value!),
+                  )),
             ),
           ],
         ),
@@ -68,7 +83,10 @@ class Body extends GetView<AccountController> {
         SizedBox(
           width: double.infinity, //biar selebar parent container
           child: ElevatedButton(
-              onPressed: () => Get.snackbar(
+              onPressed: () {
+                if (controller.validateFields()) {
+                  // Jika validasi berhasil, tampilkan snackbar sukses
+                  Get.snackbar(
                     "Sukses", // Judul snackbar
                     "User baru telah ditambahkan!", // Isi pesan
                     snackPosition: SnackPosition.BOTTOM, // Posisinya di bawah
@@ -80,7 +98,24 @@ class Body extends GetView<AccountController> {
                         Duration(seconds: 2), // Snackbar muncul selama 2 detik
                     icon: Icon(Icons.check_circle,
                         color: Colors.white), // Ikon di snackbar
-                  ),
+                  );
+                } else {
+                  // Jika ada field kosong, tampilkan snackbar error
+                  Get.snackbar(
+                    "Gagal", // Judul snackbar
+                    "Isi dulu semua data!", // Isi pesan
+                    snackPosition: SnackPosition.BOTTOM, // Posisinya di bawah
+                    backgroundColor: Colors.red, // Warna background
+                    colorText: Colors.white, // Warna teks
+                    borderRadius: 8,
+                    margin: EdgeInsets.all(10),
+                    duration:
+                        Duration(seconds: 2), // Snackbar muncul selama 2 detik
+                    icon: Icon(Icons.error,
+                        color: Colors.white), // Ikon di snackbar
+                  );
+                }
+              },
               child: Text(
                 "Tambah",
                 style: TextStyle(
