d57b56e5b186abec028ea37be8ba2bb1c9719ddf
g4l3win <144231516+g4l3win@users.noreply.github.com>
2025-02-09 15:31:26 +0700

    Update account_controller.dart

--- lib/modules/account/controller/account_controller.dart
+++ lib/modules/account/controller/account_controller.dart
@@ -1,13 +1,34 @@
 import 'package:get/get.dart';
 import 'package:bottom_nav/controllers/text_controller.dart';
 enum Gender { lakiLaki, perempuan }
-class AccountController extends GetxController{
+
+class AccountController extends GetxController {
+  //variabel controler untuk setiap textbox karena setiap textfield nilai yang diambil beda-beda dan biar gampang validasi
+  //texteditingcontroller itu fungsi bawaannya flutter
+  var nameController = TextEditingController();
+  var emailController = TextEditingController();
+  var birthdateController = TextEditingController();
+  var phoneController = TextEditingController();
+  var heightController = TextEditingController();
+  var weightController = TextEditingController();
   //cuma buat radio button
-  var selectedGender = Gender.lakiLaki.obs; // Obs untuk reactive state defaultnya laki-laki
+  var selectedGender =
+      Gender.lakiLaki.obs; // Obs untuk reactive state defaultnya laki-laki
   //late TextController txtController;// untuk buat variabel text controller yang diinisialisasi
 
-  void setSelected(Gender value){
+  void setSelected(Gender value) {
     selectedGender.value = value;
+  }
+
+  //validasi textbox controller. kalau semua text fieldnya enggak kosong hasilnya akan True
+  bool validateFields() {
+    //.text karena menyimpan value string yang diketik user
+    return nameController.text.isNotEmpty &&
+        emailController.text.isNotEmpty &&
+        birthdateController.text.isNotEmpty &&
+        phoneController.text.isNotEmpty &&
+        heightController.text.isNotEmpty &&
+        weightController.text.isNotEmpty;
   }
 
   //template untuk controller masa yang akan datang aja
