68746d2b4b7e4ff1cd57f82aadab94526bc9430b
g4l3win <144231516+g4l3win@users.noreply.github.com>
2025-02-12 16:31:59 +0700

    Add files via upload

--- lib/navigation/pages.dart
+++ lib/navigation/pages.dart
@@ -10,36 +10,37 @@ import 'package:bottom_nav/modules/settings/pages/settings_page.dart';
 import 'package:bottom_nav/controllers/text_controller_binding.dart';
 import 'package:bottom_nav/controllers/date_binding.dart';
 import 'package:bottom_nav/modules/FAQ/pages/FAQ_page.dart';
-import 'package:simple_app_android/modules/halEmpat/pages/halEmpatPage.dart';
-import 'package:simple_app_android/modules/halEmpat/binding/halEmpat_binding.dart';
+import 'package:bottom_nav/modules/FAQ/binding/FAQ_binding.dart';
+import 'package:bottom_nav/modules/halEmpat/pages/halEmpatPage.dart';
+import 'package:bottom_nav/modules/halEmpat/binding/halEmpat_binding.dart';
+import 'package:bottom_nav/modules/halEmpat/pages/cartPage.dart';
+import 'package:bottom_nav/modules/LogIn/pages/LogIn.dart';
 import 'routes.dart';
 
-class AppPages {
+class AppPages{
   AppPages._();
   //menunjukkan rute awal. dapat rutenya dari routes.dart yang udah diimport
   static const initial = Routes.home;
   //static biar bisa akses tanpa harus buat instance objek
   //const untuk optimisasi performa karena nilai initialnya itu enggak berubah selama runtime jadi g disimpan dart selama compile
 
+
   static final routes = [
-    GetPage(name: Routes.home, page: () => HomePage(), bindings: [
-      HomeBinding(),
-      SettingsBinding(),
-    ] //settings lokasinya ada di dalam home juga jadi harus di inisialisasi juga di halaman pages
-        ),
-    GetPage(
-        name: Routes.account,
-        page: () => AccountPage(),
-        binding: AccountBinding()),
-    GetPage(
-        name: Routes.browse,
-        page: () => BrowsePage(),
-        bindings: [BrowseBinding(), TextControllerBinding(), DateBinding()]),
-    GetPage(
-        name: Routes.settings,
-        page: () => SettingsPage(),
-        binding: SettingsBinding()),
-    GetPage(name: Routes.FAQ, page: () => FAQPage(), binding: FAQBinding()),
-    GetPage(name: Routes.Halempatpage, page: () => Halempatpage(), binding: HalempatBinding()),
+    GetPage(name: Routes.home, page: ()=> HomePage(), bindings: [HomeBinding(), SettingsBinding(), BrowseBinding(),HalempatBinding()] //settings lokasinya ada di dalam home juga jadi harus di inisialisasi juga di halaman pages
+    ),
+    GetPage(name: Routes.account, page: ()=> AccountPage(), binding: AccountBinding()
+    ),
+    GetPage(name: Routes.browse, page: ()=> BrowsePage(), bindings: [BrowseBinding(), TextControllerBinding(), DateBinding()]
+    ),
+    GetPage(name: Routes.settings, page: ()=> SettingsPage(), binding: SettingsBinding()
+    ),
+    GetPage(name: Routes.FAQ, page: () => FAQPage(), binding: FAQBinding()
+    ),
+    GetPage(name: Routes.halEmpat, page: () => Halempatpage(), binding: HalempatBinding()
+    ),
+    GetPage(name: Routes.cart, page: () => cartPage(),binding: HalempatBinding()
+    ),
+    GetPage(name: Routes.login, page: () => LogInPage()
+    ),
   ];
 }
--- lib/navigation/routes.dart
+++ lib/navigation/routes.dart
@@ -4,7 +4,9 @@
   static const browse = _Paths.browse;
   static const settings = _Paths.settings;
   static const FAQ = _Paths.FAQ;
-  static const Halempatpage = _Paths.halEmpatPage;
+  static const halEmpat = _Paths.halEmpat;
+  static const cart = _Paths.cart;
+  static const login = _Paths.login;
 }
 
 abstract class _Paths {
@@ -13,5 +15,7 @@ abstract class _Paths {
   static const browse = '/browse';
   static const settings = '/settings';
   static const FAQ = '/FAQ';
-  static const halEmpatPage = '/halEmpatPage'; // Perbaiki ini
+  static const halEmpat = '/halEmpat';
+  static const cart = '/cart';
+  static const login = '/login';
 }
