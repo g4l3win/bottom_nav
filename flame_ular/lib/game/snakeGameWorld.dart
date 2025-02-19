fa3fd2137d4ca32829028a13468dae263d340dcd
g4l3win <144231516+g4l3win@users.noreply.github.com>
2025-02-19 21:46:51 +0700

    Update snakeGameWorld.dart

--- flame_ular/lib/game/snakeGameWorld.dart
+++ flame_ular/lib/game/snakeGameWorld.dart
@@ -3,16 +3,23 @@ import 'package:flame/game.dart';
 import 'package:flame/components.dart';
 import 'sprites/ular.dart';
 import 'package:flame_ular/game/gridMap.dart';
+import 'package:flame_ular/game/SnakeController.dart;
 import 'snakeGame.dart';
+
 class snakeGameWorld extends World with HasGameRef<snakeGame>{
   late Ular ular;
   late GridMap gridMap;
+  late SnakeController controller;
+  
   @override
   Future<void> onLoad() async {
     gridMap = GridMap(10, 10, gameRef.size.x/10);
     //so the world would linked up to our game
     ular = Ular(gridMap);
+    controller = SnakeController(ular);
+    
     add(ular);
+    add(controller); //tambah controller 
   }
 }
 
